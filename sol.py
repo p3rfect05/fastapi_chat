8f85045823e0d5fec227d7a46a2194f940d56436
alex <sharpiron@mail.ru>
2024-02-19 20:58:37 +0300

    added migration

--- sol.py
+++ sol.py
@@ -1,45 +1,70 @@
-import math
-from sys import maxsize
-from itertools import permutations
+n = int(input())
 
-def tsp(graph, s):
-    V = len(graph)
-    vertex = []
-    for i in range(V):
-        if i != s:
-            vertex.append(i)
-    min_cost = maxsize
-    next_permutation=permutations(vertex)
-    for i in next_permutation:
-        current_cost = 0
-        k = s
-        for j in i:
-            current_cost += graph[k][j]
-            k = j
-        current_cost += graph[k][s]
-        min_cost = min(min_cost, current_cost)
-    return min_cost
-#graph1 = [[0, 10, 15, 20], [10, 0, 35, 25], [15, 35, 0, 30], [20, 25, 30, 0]] # РґР»СЏ РїСЂРѕРІРµСЂРєРё (РїРѕСЃРјРѕС‚СЂРµС‚СЊ РєР°СЂС‚РёРЅРєСѓ)
-graph2 = [[math.inf, 8, 4, 10], [8, math.inf, 7, 5], [4, 7, math.inf, 3], [10, 5, 3, math.inf]]
+pts = []
+visited = {}
+res = n
+b_a = {}
+for i in range(n):
+    a, b = map(int, input().split(' '))
+    # if (0, a) in visited or (1, b) in visited:
+    #     if (0, a) in visited and visited[(0, a)] == 1:
+    #         res -= 2
+    #     elif (0, a) in visited:
+    #         res -= 1
+    #
+    #     if (1, b) in visited and visited[(1, b)] == 1:
+    #         res -= 2
+    #     elif (1, b) in visited:
+    #         res -= 1
+    #
+    # visited[(0, a)] = 1 + visited.get((0, a), 0)
+    # visited[(1, b)] = 1 + visited.get((1, b), 0)
+    if (1, b) in b_a:
+        b_a[(1, b)].append((0, a))
+    else:
+        b_a[(1, b)] = [(0, a)]
+    pts.append([(0, a), (1, b)])
 
-adj1 = [
-    [math.inf, 7, 12, 25, 10],
-    [10, math.inf, 9, 5, 11],
-    [13, 8, math.inf, 6, 4],
-    [6, 11, 15, math.inf, 15],
-    [5, 9, 12, 17, math.inf]
-]
-adj2 = [
-    [math.inf, 7, 11, 10, 13, 10],
-    [15, math.inf, 14, 10, 12, 6],
-    [9, 6, math.inf, 10, 1, 6],
-    [19, 7, 5, math.inf, 13, 13],
-    [4, 7, 5, 10, math.inf, 11],
-    [8, 7, 3, 10, 4, math.inf]
-]
-print(tsp(adj2, 0))
-'''
-Р—Р°РґР°С‡Р° РєРѕРјРјРёРІРѕСЏР¶С‘СЂР° (Travelling Salesman Problem, TSP) вЂ” Р·Р°РґР°С‡Р° РєРѕРјР±РёРЅР°С‚РѕСЂРЅРѕР№ РѕРїС‚РёРјРёР·Р°С†РёРё. 
-РљР°Рє РїСЂР°РІРёР»Рѕ, РµС‘ СЃСѓС‚СЊ СЃРІРѕРґРёС‚СЃСЏ Рє РїРѕРёСЃРєСѓ РѕРїС‚РёРјР°Р»СЊРЅРѕРіРѕ РїСѓС‚Рё, 
-РїСЂРѕС…РѕРґСЏС‰РµРіРѕ С‡РµСЂРµР· РІСЃРµ РїСЂРѕРјРµР¶СѓС‚РѕС‡РЅС‹Рµ РїСѓРЅРєС‚С‹ РїРѕ РѕРґРЅРѕРјСѓ СЂР°Р·Сѓ Рё РІРѕР·РІСЂР°С‰Р°СЋС‰РµРіРѕСЃСЏ РІ РёСЃС…РѕРґРЅСѓСЋ С‚РѕС‡РєСѓ.
-'''
+
+pts.sort()
+#print(pts)
+#b = [pts[0][1]]
+#b_i = 0
+b_stack = [pts[0][1]]
+max_b = pts[0][1]
+i = 1
+#print(pts)
+count1 = {pts[0][0] : 1}
+while i < len(pts):
+    #print(b_stack, res)
+
+    cur_b = pts[i][1]
+    count1[pts[i][0]] = 1 + count1.get(pts[i][0], 0)
+    #print(b_stack, cur_b, max_b)
+    if b_stack and cur_b <= b_stack[-1]:
+        res -= 1
+        while b_stack and cur_b <= b_stack[-1]:
+            #max_b = max(max_b, cur_b)
+            res -= 1
+            b_stack.pop()
+    elif not b_stack and cur_b <= max_b:
+        res -= 1
+    else:
+        #print(cur_b)
+        b_stack.append(cur_b)
+    #print(b_stack, cur_b, max_b)
+    max_b = max(max_b, cur_b)
+    i += 1
+    #print(b_stack, res)
+
+
+count2 = {}
+for b in b_stack:
+    for i in b_a[b]:
+        count2[i] = 1 + count2.get(i, 0)
+for c in count1:
+    if c in count2 and count1[c] > 1:
+        res -= count2[c]
+#print(count1, count2)
+#print(b_stack)
+print(res)
