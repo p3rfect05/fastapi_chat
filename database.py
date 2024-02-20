2b5e56842b9adb3bd4a5000645afddb7965d4d3a
Alexander <77683293+p3rfect05@users.noreply.github.com>
2024-02-20 12:54:28 +0000

    sslmode require

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from config import POSTGRES_USER, POSTGRES_DB, POSTGRES_PASSWORD, DB_HOST, DB_PO
 
 DATABASE_URL = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}?ssl=true'
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, connect_args={'sslmode': "require"})
 
 print('engine created')
 async_sessionmaker = async_sessionmaker(
