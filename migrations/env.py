65208f5f1a69933a4227e3603a5dc72cb09bef8f
alex <sharpiron@mail.ru>
2024-03-08 00:04:27 +0300

    changed alembic to async mode

--- migrations/env.py
+++ migrations/env.py
@@ -1,18 +1,18 @@
-import os
+import asyncio
 import sys
 from logging.config import fileConfig
 from os.path import dirname, abspath
 
-from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 from database import Base, DATABASE_URL
 from chat.models import *
+sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-#basedir = os.path.abspath(os.path.dirname(__file__))
 config = context.config
 config.set_main_option('sqlalchemy.url', f"{DATABASE_URL}?async_fallback=True")
 # Interpret the config file for Python logging.
@@ -56,26 +56,35 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
 
-    In this scenario we need to create an Engine
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations() -> None:
+    """In this scenario we need to create an Engine
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
+
+    connectable = async_engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
 
-        with context.begin_transaction():
-            context.run_migrations()
+    await connectable.dispose()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+
+    asyncio.run(run_async_migrations())
 
 
 if context.is_offline_mode():
--- migrations/versions/b3d2b618ba64_.py
+++ migrations/versions/b3d2b618ba64_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 16a073005010
+Revision ID: b3d2b618ba64
 Revises: 
-Create Date: 2024-02-20 14:41:12.113305
+Create Date: 2024-03-08 00:03:43.561609
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '16a073005010'
+revision: str = 'b3d2b618ba64'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
